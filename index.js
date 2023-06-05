199bdb5dc5f6ab7ae90c590373a5044a463368be
srashtiiii <srashtig03@gmail.com>
2023-06-05 05:59:57 +0530

    day2

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const generalBtn=document.getElementById("general");
-const businessBtn=document.getElementById("general");
-const sportsBtn=document.getElementById("general");
-const entertainmentBtn=document.getElementById("general");
-const technologyBtn=document.getElementById("general");
-const searchBtn=document.getElementById("general");
+const businessBtn=document.getElementById("business");
+const sportsBtn=document.getElementById("sports");
+const entertainmentBtn=document.getElementById("entertainment");
+const technologyBtn=document.getElementById("technology");
+const searchBtn=document.getElementById("searchBtn");
+
+const newsQuery=document.getElementById("newsQuery")
+const newsType=document.getElementById("newsType");
+
+
+generalBtn.addEventListener("click",function()
+{
+
+});
+
+
+businessBtn.addEventListener("click",function()
+{
+
+});
+
+
+sportsBtn.addEventListener("click",function()
+{
+
+});
+
+entertainmentBtn.addEventListener("click",function()
+{
+
+});
+
+
+technologyBtn.addEventListener("click",function()
+{
+
+});
+
+
+searchBtn.addEventListener("click",function()
+{
+
+});
+
