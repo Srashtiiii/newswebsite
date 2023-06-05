ae36a81c81a78f6deb9866a5ed8f09aea7e87706
srashtiiii <srashtig03@gmail.com>
2023-06-05 05:38:20 +0530

    day3

---
+++ index.js
@@ -0,0 +1,6 @@
+const generalBtn=document.getElementById("general");
+const businessBtn=document.getElementById("general");
+const sportsBtn=document.getElementById("general");
+const entertainmentBtn=document.getElementById("general");
+const technologyBtn=document.getElementById("general");
+const searchBtn=document.getElementById("general");
